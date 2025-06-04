7c4c4e73ed7b71c0b0f6bbb71c28542c4cab32a2
daniil-b2be <dan.developer@born2be.media>
2025-06-04 10:20:34 +0300

    price

--- src/features/package/components/download-price.tsx
+++ src/features/package/components/download-price.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 
 import { LinesSection } from '@/shared/ui/components/lines-section';
@@ -14,10 +15,12 @@ export const DownloadPrice = () => {
       title={t('title')}
       text={t('description')}
       bottomContent={
-        <Button>
-          {t('btn')}
-          <ArrowRightIcon />
-        </Button>
+        <Link href="/price_list_optisence.pdf">
+          <Button>
+            {t('btn')}
+            <ArrowRightIcon />
+          </Button>
+        </Link>
       }
     />
   );
